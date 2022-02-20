db9bd5d547dc1f391999a90d08e09ff55dc8139a
Sergey Kovalev <dip.deep@mail.ru>
2022-02-20 23:07:52 +0300

    modified Delivery class

--- src/Delivery.java
+++ src/Delivery.java
@@ -1,11 +1,9 @@
 import java.time.LocalTime;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
@@ -31,45 +29,45 @@ public class Delivery {
 
         @Override
         public void run() {
-            try {
-                while (true) {
-//                    Iterator<Order> orderIterator = orderQueue.iterator();
-//                    LocalTime earlyDeliveryTime = LocalTime.now();
-                    int earlyDeliveryIndex = 0;
-//                    while (orderIterator.hasNext()) {
-//                        LocalTime currentDeliveryTime = orderIterator.next().getFinalDeliveryTime();
-//                        if (currentDeliveryTime.isBefore(earlyDeliveryTime)) earlyDeliveryTime = currentDeliveryTime;
-//                    }
-                    for (int i = 0; i < orderQueue.size(); i++) {
-                        LocalTime currentDeliveryTime = orderQueue.take().getFinalDeliveryTime();
+            while (true) {
+                try {
+                    int earlyDeadlineIndex = 0;
+                    LocalTime earlyDeadlineTime = orderList.get(earlyDeadlineIndex).getDeadlineTime();
+                    for (int i = 0; i < orderList.size(); i++) {
+                        LocalTime orderDeadlineTime = orderList.get(i).getDeadlineTime();
+                        if (orderDeadlineTime.isBefore(earlyDeadlineTime)) {
+                            earlyDeadlineTime = orderDeadlineTime;
+                            earlyDeadlineIndex = i;
+                        }
                     }
+                    Order order = orderList.get(earlyDeadlineIndex);
 
-                    Order order = orderQueue.poll(500, TimeUnit.MILLISECONDS);
+//                    Order order = orderQueue.poll(500, TimeUnit.MILLISECONDS);
                     if (order != null) {
                         String orderID = order.orderID;
-                        System.out.println("The car " + carID + "took the order #" + orderID);
-                        int deliveryTime = 500 + (int) (Math.random() * 500);
-                        sleep(deliveryTime);
-                        System.out.println("The car " + carID + "delivered the order #" + orderID);
+                        System.out.println("The car#" + carID + "took the order #" + orderID);
+                        sleep(500 + (int) (Math.random() * 500));
+                        System.out.println("The car#" + carID + "delivered the order #" + orderID);
                     }
+                } catch (InterruptedException e) { // if interrupted while waiting
+//                    e.printStackTrace();
+                    break;
                 }
-            } catch (InterruptedException e) { // if interrupted while waiting
-                e.printStackTrace();
             }
         }
     }
 
     private static class Order {
         private final String orderID;
-        private final LocalTime finalDeliveryTime;
+        private final LocalTime deadlineTime;
 
         public Order(int timeForDeliveryInSeconds) {
-            orderID = UUID.randomUUID().toString();
-            finalDeliveryTime = LocalTime.now().plusSeconds(timeForDeliveryInSeconds);
+            orderID = UUID.randomUUID().toString().substring(0, 7);
+            deadlineTime = LocalTime.now().plusSeconds(timeForDeliveryInSeconds);
         }
 
-        public LocalTime getFinalDeliveryTime() {
-            return finalDeliveryTime;
+        public LocalTime getDeadlineTime() {
+            return deadlineTime;
         }
     }
 
@@ -78,7 +76,7 @@ public class Delivery {
             while (true) {
                 try {
                     Order o = new Order(1 + (int) (Math.random() * 2));
-                    orderQueue.add(o);
+//                    orderQueue.add(o);
                     orderList.add(o);
                     sleep(400);
                 } catch (InterruptedException e) {
